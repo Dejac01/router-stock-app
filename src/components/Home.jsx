6d02a978d92c7a0e82ea44b4060294da36538a6a
deja.craft <dejacraft28@gmail.com>
2023-03-21 22:29:05 -0400

    Stock-App

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Stocks from './Stocks'
 
-function Home() {
+function Home(props) {
   return (
-    <div>Home</div>
+    <div>
+      <Stocks/>
+    </div>
   )
 }
 
