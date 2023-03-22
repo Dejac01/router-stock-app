f3c54e58f3abf89d44f35d552c1bfbaab8446e2d
deja.craft <dejacraft28@gmail.com>
2023-03-21 21:44:46 -0400

    Stock-App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import Stocks from "./pages/Stocks";
+import Stocks from "./components/Stocks";
 import Nav from './components/Nav';
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
- 
+    <Nav/>
+
+<Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/currencies" element={<Currencies/>}/>
+  <Route path="/stocks/:symbols" element={<Stocks/>}/>
+</Routes>
     </div>
   );
 }
